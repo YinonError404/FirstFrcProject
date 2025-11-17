c057022a4060c589dcdd5f4118581517d66d1b39
YinonError404 <ynnon.dahan@gmail.com>
2025-11-17 19:17:25 +0200

    Deleted unnecessarry files
    deleted the "Wheel" branch because it was in the arm branch and it should not be.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,6 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.subsystems.Wheel.Wheel;
 import frc.robot.subsystems.arm.Arm;
 
 
