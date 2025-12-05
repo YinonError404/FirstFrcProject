f62e2b337ee08e916111a176ac565d664f0defff
YinonError404 <ynnon.dahan@gmail.com>
2025-12-05 14:07:56 +0200

    Revert "Merge pull request #2 from yinondahan/tank"
    This reverts commit 2ea57b37c52244b4cd1d170e84818068d4aa4b67, reversing
    changes made to 8b0b195a877c3dae2f03ec50c5a3cfa11a286133.

---
+++ src/main/java/frc/robot/subsystems/Wheel/WheelCommands.java
@@ -0,0 +1,38 @@
+package frc.robot.subsystems.Wheel;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+
+public class WheelCommands {
+    public static Command getCollectCommands() {
+        return new StartEndCommand(
+                () -> CallWheel.wheel.collect(),
+                () -> CallWheel.wheel.stop(),
+                CallWheel.wheel
+        );
+    }
+
+    public static Command getEjectCommands() {
+        return new StartEndCommand(
+                () -> CallWheel.wheel.eject(),
+                () -> CallWheel.wheel.stop(),
+                CallWheel.wheel
+        );
+    }
+
+    public static Command getCollectThenEjectCommand() {
+        return new SequentialCommandGroup(
+                getCollectCommands().withTimeout(3),
+                getEjectCommands().withTimeout(3),
+                new InstantCommand(
+                        () -> CallWheel.wheel.stop(),
+                        CallWheel.wheel
+
+                )
+
+        );
+    }
+
+}
