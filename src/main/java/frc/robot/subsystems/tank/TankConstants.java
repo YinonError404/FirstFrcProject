bee2385f00341729f4022f96ec12a5e840a3a5ec
YinonError404 <ynnon.dahan@gmail.com>
2025-12-05 13:42:21 +0200

    FIXED NAMES

--- src/main/java/frc/robot/subsystems/tank/TankConstants.java
+++ src/main/java/frc/robot/subsystems/tank/TankConstants.java
@@ -6,13 +6,13 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class TankConstants {
-    private static final int LEFT_MOTOR_1_ID = 1, LEFT_MOTOR_2_ID = 2 ,RIGHT_MOTOR_1_ID = 3, RIGHT_MOTOR_2_ID = 4;
-    static final WPI_TalonSRX LEFT_MOTOR_1 = new WPI_TalonSRX(LEFT_MOTOR_1_ID);
-    static final WPI_TalonSRX LEFT_MOTOR_2 = new WPI_TalonSRX(LEFT_MOTOR_2_ID);
-    static final WPI_TalonSRX RIGHT_MOTOR_1= new WPI_TalonSRX(RIGHT_MOTOR_1_ID);
-    static final WPI_TalonSRX RIGHT_MOTOR_2 = new WPI_TalonSRX(RIGHT_MOTOR_2_ID);
+    private static final int LEFT_MASTER_ID = 1, LEFT_FOLLOWER_ID = 2 ,RIGHT_MASTER_ID = 3, RIGHT_FOLLOWER_ID = 4;
+    static final WPI_TalonSRX LEFT_MASTER = new WPI_TalonSRX(LEFT_MASTER_ID);
+    static final WPI_TalonSRX LEFT_FOLLOWER = new WPI_TalonSRX(LEFT_FOLLOWER_ID);
+    static final WPI_TalonSRX RIGHT_MASTER= new WPI_TalonSRX(RIGHT_MASTER_ID);
+    static final WPI_TalonSRX RIGHT_FOLLOWER = new WPI_TalonSRX(RIGHT_FOLLOWER_ID);
 
-    static final DifferentialDrive DIFFERENTIAL_DRIVE = new DifferentialDrive(LEFT_MOTOR_1, RIGHT_MOTOR_1);
+    static final DifferentialDrive DIFFERENTIAL_DRIVE = new DifferentialDrive(LEFT_MASTER, RIGHT_MASTER);
     private static final double VOLTAGE_LIMIT = 12;
 
     private static final boolean LEFT_MOTOR_1_INVERTED_VALUE = true;
@@ -28,31 +28,31 @@ public class TankConstants {
 
 
     private static void configLeftMotors() {
-        LEFT_MOTOR_1.configFactoryDefault();
-        LEFT_MOTOR_2.configFactoryDefault();
-        LEFT_MOTOR_1.enableVoltageCompensation(true);
-        LEFT_MOTOR_2.enableVoltageCompensation(true);
-        LEFT_MOTOR_1.configVoltageCompSaturation(VOLTAGE_LIMIT);
-        LEFT_MOTOR_2.configVoltageCompSaturation(VOLTAGE_LIMIT);
-        LEFT_MOTOR_1.setInverted(LEFT_MOTOR_1_INVERTED_VALUE);
-        LEFT_MOTOR_2.setInverted(LEFT_MOTOR_2_INVERTED_VALUE);
-        LEFT_MOTOR_1.setNeutralMode(NEUTRAL_MODE);
-        LEFT_MOTOR_2.setNeutralMode(NEUTRAL_MODE);
-        LEFT_MOTOR_2.follow(LEFT_MOTOR_1, FollowerType.PercentOutput);
+        LEFT_MASTER.configFactoryDefault();
+        LEFT_FOLLOWER.configFactoryDefault();
+        LEFT_MASTER.enableVoltageCompensation(true);
+        LEFT_FOLLOWER.enableVoltageCompensation(true);
+        LEFT_MASTER.configVoltageCompSaturation(VOLTAGE_LIMIT);
+        LEFT_FOLLOWER.configVoltageCompSaturation(VOLTAGE_LIMIT);
+        LEFT_MASTER.setInverted(LEFT_MOTOR_1_INVERTED_VALUE);
+        LEFT_FOLLOWER.setInverted(LEFT_MOTOR_2_INVERTED_VALUE);
+        LEFT_MASTER.setNeutralMode(NEUTRAL_MODE);
+        LEFT_FOLLOWER.setNeutralMode(NEUTRAL_MODE);
+        LEFT_FOLLOWER.follow(LEFT_MASTER, FollowerType.PercentOutput);
 
     }
 
     private static void configRightMotors() {
-        RIGHT_MOTOR_1.configFactoryDefault();
-        RIGHT_MOTOR_2.configFactoryDefault();
-        RIGHT_MOTOR_1.enableVoltageCompensation(true);
-        RIGHT_MOTOR_2.enableVoltageCompensation(true);
-        RIGHT_MOTOR_1.configVoltageCompSaturation(VOLTAGE_LIMIT);
-        RIGHT_MOTOR_2.configVoltageCompSaturation(VOLTAGE_LIMIT);
-        RIGHT_MOTOR_1.setInverted(RIGHT_MOTOR_1_INVERTED_VALUE);
-        RIGHT_MOTOR_2.setInverted(RIGHT_MOTOR_2_INVERTED_VALUE);
-        RIGHT_MOTOR_1.setNeutralMode(NEUTRAL_MODE);
-        RIGHT_MOTOR_2.setNeutralMode(NEUTRAL_MODE);
-        RIGHT_MOTOR_2.follow(RIGHT_MOTOR_1,FollowerType.PercentOutput);
+        RIGHT_MASTER.configFactoryDefault();
+        RIGHT_FOLLOWER.configFactoryDefault();
+        RIGHT_MASTER.enableVoltageCompensation(true);
+        RIGHT_FOLLOWER.enableVoltageCompensation(true);
+        RIGHT_MASTER.configVoltageCompSaturation(VOLTAGE_LIMIT);
+        RIGHT_FOLLOWER.configVoltageCompSaturation(VOLTAGE_LIMIT);
+        RIGHT_MASTER.setInverted(RIGHT_MOTOR_1_INVERTED_VALUE);
+        RIGHT_FOLLOWER.setInverted(RIGHT_MOTOR_2_INVERTED_VALUE);
+        RIGHT_MASTER.setNeutralMode(NEUTRAL_MODE);
+        RIGHT_FOLLOWER.setNeutralMode(NEUTRAL_MODE);
+        RIGHT_FOLLOWER.follow(RIGHT_MASTER,FollowerType.PercentOutput);
     }
 }
